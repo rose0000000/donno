6ac9835327c437db7c3da63d881b7b5ea8bc2302
rose0000000 <rosevilleiglesia@gmail.com>
2025-02-04 23:23:27 +0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,1 @@
-import os
-from pathlib import Path
-os.system("echo 'thenga' > echo 'thenga' > C:\Users\%USERNAME%\AppData\Local\Temp\zzhahahha.txt")
-
-file_path = Path("newfile.txt")
-file_path.write_text("This is a new text file using pathlib.")
 
